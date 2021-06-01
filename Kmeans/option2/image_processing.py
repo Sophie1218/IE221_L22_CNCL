86c89140396232f0bb6754f52b4027dd0ff9e055
Sophie1218 <75886095+Sophie1218@users.noreply.github.com>
2021-06-01 09:36:18 +0700

    Edit

--- Kmeans/option2/image_processing.py
+++ Kmeans/option2/image_processing.py
@@ -1,129 +1,61 @@
 # import libraries
-import myprogram.interface as itf
-from myprogram.model import kmeans
-import pygame
-from myprogram.option2.image_processing import Image
 import matplotlib.pyplot as plt
-import numpy as np
+from pygame.image import load
+from pygame.transform import scale
 
 
-def option2():
+class Image:
     """
-    interface option 2
+     A class to represent a Image object.
 
-            Parameters:
+     ...
 
+     Attributes
+     ----------
+     path : string
+         path of image
 
-            Returns:
-                    status of option 2: True/False
-                    True: option 2 is running
-                    False: option 2 is closed
-    """
-    pygame.init()
-    screen = itf.screen()
+     Methods
+     -------
+    process_image(self):
+         read image by matplotlib and return the number matrix with 3 dimension
+    return_size(self):
+         resize image
+    update_label(label):
+         update label after running k - means model
+    show_image(self, screen):
+        show image on screen
+    return_path(self):
+         return path of image
+    update_path(self, path):
+        update path of image
 
-    itf.caption()
+     """
+    def __init__(self, path):
+        self.path = path
 
-    clock = pygame.time.Clock()
-    K = 0
-    shiftDown = False
-    image = Image(None)
-    running = True
-    insert = itf.TextBox('Insert the image path', (850, 50), itf.BLUE, itf.BACKGROUND)
-    input_box = itf.InputTextBox('', (850, 150), itf.BLACK, itf.BLACK)
-    input_box.update_width_rect(300)
-    show_image = itf.TextBox('Show', (950, 225), itf.WHITE, itf.BLACK)
-    plus = itf.TextBox('+', (900, 300), itf.WHITE, itf.BLACK)
-    minus = itf.TextBox('-', (950, 300), itf.WHITE, itf.BLACK)
-    K_text = itf.TextBox('K = ' + str(K), (1000, 300), itf.BLACK, itf.BACKGROUND)
-    run = itf.TextBox('Run and Show result', (850, 400), itf.WHITE, itf.BLACK)
+    def process_image(self):
+        img = plt.imread(self.path)
+        return img.reshape(img.shape[0] * img.shape[1], 3)
 
-    while running:
-        clock.tick(60)
-        screen.fill(itf.BACKGROUND)
+    def return_size(self):
+        img = plt.imread(self.path)
+        return [img.shape[0], img.shape[1]]
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if input_box.is_click(event.pos):
-                    input_box.update_active(True)
-                else:
-                    input_box.update_active(False)
+    def show_image(self, screen):
+        max_width = 750
+        max_height = 600
+        image = load(self.path)
+        if image.get_height() > 650:
+            ratio = min(max_width / image.get_width(), max_height / image.get_height())
+            image = scale(image, (int(image.get_width() * ratio), int(image.get_height() * ratio)))
+        if image.get_width() > 750:
+            ratio = min(max_width / image.get_width(), max_height / image.get_height())
+            image = scale(image, (int(image.get_width() * ratio), int(image.get_height() * ratio)))
+        screen.blit(image, (50, 50))
 
-                # Change K button +
-                if plus.is_click(event.pos):
-                    K += 1
-                    K_text.update_text('K = ' + str(K))
-                print('K+ button pressed')
+    def return_path(self):
+        return self.path
 
-                # Change K button -
-                if minus.is_click(event.pos):
-                    if K > 0:
-                        K -= 1
-                        K_text.update_text('K = ' + str(K))
-                print('K- button pressed')
-
-                # Press show button
-                if show_image.is_click(event.pos):
-                    if len(input_box.return_content()) > 0:
-                        image.update_path(input_box.return_content())
-                print('Show button pressed')
-
-                # Press run button and show image after running K - means
-                if run.is_click(event.pos):
-                    if K > 0 and None != image.return_path():
-
-                        img = image.process_image()
-                        model = kmeans(n_clusters=K).fit(img)
-                        labels = model.predict(img)
-                        clusters = model.cluster_centers_
-
-                        img2 = np.zeros_like(img)
-
-                        for i in range(len(img2)):
-                            img2[i] = clusters[labels[i]]
-
-                        img2 = img2.reshape(image.return_size()[0], image.return_size()[1], 3)
-                        plt.imshow(img2)
-                        plt.show()
-
-                    print('Run button pressed')
-
-            if event.type == pygame.KEYUP:
-                if event.key in [pygame.K_RSHIFT, pygame.K_LSHIFT]:
-                    shiftDown = False
-            if event.type == pygame.KEYDOWN:
-                if event.key in [pygame.K_RSHIFT, pygame.K_LSHIFT]:
-                    shiftDown = True
-                if input_box.is_active():
-                    if event.key == pygame.K_BACKSPACE:
-                        input_box.minus_chr()
-                    else:
-                        input_box.add_chr(pygame.key.name(event.key), shiftDown)
-                    input_box.update_width_rect(max(300, input_box.get_text_width() + 10))
-        if None != image.return_path():
-            try:
-                image.show_image(screen)
-            except:
-                print("Error")
-        # Draw interface
-        # Input box for inserting path image
-        input_box.show(screen)
-        # Text 'Insert the path image'
-        insert.show(screen)
-        # Show button
-        show_image.show(screen)
-        # K button +
-        plus.show(screen)
-        # K button -
-        minus.show(screen)
-        # K value
-        K_text.show(screen)
-        # Run button
-        run.show(screen)
-        # End draw interface
-        pygame.display.flip()
-
-    pygame.quit()
-    return running
+    def update_path(self, path):
+        self.path = path
